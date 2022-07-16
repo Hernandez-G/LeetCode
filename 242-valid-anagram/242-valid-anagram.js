d2a131e07c81ecbbbf2c204cc8aeca7d3601672a
Gaby Hernandez <101479437+Hernandez-G@users.noreply.github.com>
2022-07-15 20:43:37 -0700

    Time: 84 ms (92.92%), Space: 44.1 MB (56.53%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) {
+        return false;
+    }
+    let letterCount = {};
+    
+    for(let letter of s) {
+        if(letterCount[letter]) {
+            letterCount[letter] ++;
+        } else {
+            letterCount[letter] = 1;
+        }
+    }
+    
+    for(let letter of t) {
+        if(letterCount[letter] === undefined) {
+            return false;
+        }
+        
+        if(letterCount[letter] < 1) {
+            return false;
+        }
+        letterCount[letter]--;
+    }
+    return true;
+    
+};
