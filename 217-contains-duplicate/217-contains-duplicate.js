948b6a347ef15577ee05ab6099a5e3d959cd7706
Gaby Hernandez <101479437+Hernandez-G@users.noreply.github.com>
2022-07-15 20:22:06 -0700

    Time: 148 ms (41.93%), Space: 51 MB (44.85%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,13 +3,32 @@
  * @return {boolean}
  */
 
+
+// SOLUTION 1
 // Space: O(n)
+// var containsDuplicate = function(nums) {
+//     let hashtable = {};
+//     for(let i = 0; i < nums.length; i++) {
+//         if (nums[i] in hashtable)
+//             return true;
+//         hashtable[nums[i]] = 1;
+//     }
+//     return false
+// };
+
+ 
+//SOLUTION 2:  https://www.youtube.com/watch?v=umqL2CyEywM
 var containsDuplicate = function(nums) {
-    let hashtable = {};
-    for(let i = 0; i < nums.length; i++) {
-        if (nums[i] in hashtable)
-            return true;
-        hashtable[nums[i]] = 1;
+    
+    var numbers = new Set();
+
+for(let num of nums) {
+//     if the set doesnt already have the number
+    if(!numbers.has(num)) {
+        numbers.add(num);
+    } else {
+        return true;
     }
-    return false
+}
+    return false;
 };
